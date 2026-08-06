25ea255577438127c3d0325c82f2d49f95c8915b
limjunhee <ski725400@gmail.com>
2026-08-06 15:12:44 +0900

    Day7_5

--- src/Day07/test2.java
+++ src/Day07/test2.java
@@ -6,37 +6,50 @@ public class test2 {
     public static void main(String[] args) {
        OverallRepository review_repository = new OverallRepository();
        OverallRepository post_repository = new OverallRepository();
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in, "EUC-KR");
         for (;;) {
             System.out.println("============ My Community ============ ");
-            System.out.println("1. 리뷰등록, 2. 리뷰출력, 3. 게시물등록, 4. 게시물출력");
+            System.out.println("1. 게시물등록, 2. 게시물출력, 3. 리뷰등록, 4. 리뷰출력");
             System.out.println("====================================== ");
             System.out.print("선택>");
             int ch = scan.nextInt();
 
             if (ch == 1) {
                 scan.nextLine();
-                System.out.print("리뷰내용 : ");     String content = scan.nextLine();
-                System.out.print("점수 : ");   String writer = scan.nextLine();
+                System.out.print("게시물내용 : ");    String content = scan.nextLine();
+                System.out.print("게시물작성자 : ");        String writer = scan.nextLine();
 
                 Post post = new Post(content, writer);
-                boolean result = review_repository.save(post);
+                boolean result = review_repository.postsave(post);
 
                 if (result) { System.out.println("[안내] 리뷰등록 성공");} 
                 else { System.out.println("[안내] 글쓰기 실패"); }
 
             } else if (ch == 2) {
-                Post[] posts = review_repository.findAll();
+                Post[] posts = review_repository.findAllPosts();
                 for (Post post : posts) {
                     if (post != null) {
-                        System.out.printf("리뷰내용 : %s , 점수 : %s \n", post.writer, post.content);
+                        System.out.printf("리뷰내용 : %s , 점수 : %s \n", post.content, post.writer);
                     }
                 }
             } else if (ch == 3) {
                 scan.nextLine();
-                sys
-            } else if (ch == 4) {
+                System.out.print("리뷰내용 : ");    String review = scan.nextLine();
+                System.out.print("리뷰점수 : ");        int score = scan.nextInt();
+                System.out.print("리뷰작성일 : ");        String listDay = scan.nextLine();
 
+                reviewTable post = new reviewTable(review, score, listDay);
+                boolean result = review_repository.reviewsave(post);
+
+                if (result) { System.out.println("[안내] 리뷰등록 성공"); }
+                else { System.out.println("[안내] 리뷰등록 실패"); }
+            } else if (ch == 4) {
+                reviewTable[] posts = review_repository.findAllReviews();
+                for (reviewTable post : posts) {
+                    if (post != null) {
+                        System.out.printf("리뷰내용 : %s , 리뷰점수 : %d , 리뷰작성일 : %s \n", post.review, post.score, post.listDay);
+                    }
+                }
             }
         }
     }
@@ -45,8 +58,9 @@ public class test2 {
 class OverallRepository {
 
     Post[] posts = new Post[100];
+    reviewTable[] reviewTable = new reviewTable[100];
 
-    boolean save(Post post) {
+    boolean postsave(Post post) {
         for (int index = 0; index < posts.length; index++) {
             if (posts[index] == null) {
                 posts[index] = post;
@@ -56,13 +70,28 @@ class OverallRepository {
         return false;
     }
 
-    Post[] findAll() {
+    // 리뷰테이블에 저장
+    boolean reviewsave(reviewTable revTable) {
+        for (int index = 0; index < reviewTable.length; index++) {
+            if (reviewTable[index] == null) {
+                reviewTable[index] = revTable;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    Post[] findAllPosts() {
         return posts;
     }
 
-
+    // 리뷰테이블 전체 목록 반환
+    reviewTable[] findAllReviews() {
+        return reviewTable;
+    }
 }
 
+// 게시물 
 class Post {
     String content;
     String writer;
@@ -76,7 +105,32 @@ class Post {
     }
 }
 
-class Posttable {
+
+// 리뷰테이블 class 생성
+class reviewTable {
+    String review;
+    int score;
+    String listDay;
+
+    // 기본 생성자 생성
+    reviewTable() {
+    }
+
+    reviewTable(String review, int score, String listDay) {
+        this.review = review;
+        this.score = score;
+        this.listDay = listDay;
+    }
+}
+
+
+
+
+
+
+
+
+class Post2 {
     int listID; // PK
     int memberID; // FK
     String listTitle;
@@ -98,7 +152,7 @@ class cigarTable{
     String cigarimg;
 }
 
-class reviewTable {
+class reviewTable2 {
     int reviewID;
     int CigarID;
     int MemberID;
