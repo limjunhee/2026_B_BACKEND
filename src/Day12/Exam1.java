edac6748c1f40757926ef714efe4c07758362725
limjunhee <ski725400@gmail.com>
2026-08-13 12:01:21 +0900

    Day12_3

--- src/Day12/Exam1.java
+++ src/Day12/Exam1.java
@@ -1,5 +1,8 @@
 package Day12;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Exam1 {
     public static void main(String[] args) {
         // 예외 : 에러 발생 시 고치는 게 아니라 흐름 제어
@@ -55,7 +58,43 @@ public class Exam1 {
             System.out.println( e ); // java.lang.ArrayIndexOutOfBoundsException: Index 10 out of bounds for length 2
         }
 
+        // [3] 다중 catch : try에서 다양한 예외들을 다양하게 흐름 제어
+        // [4] finally: 예외가 발생 여부 상관 없이 무조건 실행하는 구역, 외부 프로그램과 연동 종료 코드를 둠
+        // 6. InputMismatchException: 타입 예외
+        Scanner scanner = new Scanner(System.in, "EUC-KR"); 
+        try {
+            System.out.print("정수 입력:");
+            int ch = scanner.nextInt(); // 입력받은 자료를 int로 변환, 문자열 입력시 오류
+            Integer.parseInt("ABC"); // 예외
+        } catch (InputMismatchException e) {
+            System.out.println("정수만 입력하세요." + e);
+        } catch( NumberFormatException e ){ // 다중 catch() 가능!
+            System.out.println("타입 변환 오류!" + e);
+        } catch( Exception e) { // 다중 catch -> Exception으로 그 외 예외처리 실시 (단, Exception은 가장 마지막에 둘 것. 앞에 두면 자식 예외처리들은 무용지물이 됨)
+            System.out.println("예외 발생, 관리자에게 문의하세요" + e);
+        } finally {
+            System.out.println("무조건 실행되는 부분입니다.");
+        }
+
         // * 여러 타입들의 예외들을 하나의 타입으로 Exception으로 사용 가능 (Exception -> 모든 예외들의 최상위 부모 클래스다.)
         // 단, 한 예외로 퉁쳐버리기 때문에 디테일 한 부분은 파악 불가능
+
+        // * try{ 예외가 발생할 것으로 예상되는 코드 } catch( 예외클래스명 예변수명){ } finally{ }
+        
+        
+        
+        // [5] 예외 떠넘기기 , *예외 반환* : 해당 메소드를 호출한 곳으로 예외 반환
+        try {
+            method1(); // 예외가 반환, throws가 존재해서 메소드가 아니라 여기서 빨간줄이 뜬다.
+                       // main에다 throws 하면 빨간줄 없어지긴 한다. 이 경우엔 프로그램이 다운되지 않으나 흐름 제어를 못한다.    
+        } catch (Exception e) {
+            System.out.println("메소드 예외 발생! "+ e);
+        }
+        
+    } 
+
+    // 7. 예외발생시 발생한 곳에서 예외처리 하지 않고 해당 함수를 반환시킬 수 있음
+    public static void method1() throws ClassNotFoundException {
+        Class.forName("java.lang.Spring"); // 예외 발생?
     }
 }
